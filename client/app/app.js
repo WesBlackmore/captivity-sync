591a6c8849a27e0b5e97191e53a3fa1c86aed6a6
Wes Blackmore <wes.blackmore@gmail.com>
2016-07-11 13:06:48 +0200

    First: Stage files

---
+++ client/app/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('loveLabApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router'
+])
+  .config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
